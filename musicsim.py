2bd2c826f2a8003a8806f31abedfbefcd077b1b2
Dixon Styres <styresdc@users.noreply.github.com>
2015-02-11 22:51:58 -0500

    added musicsimcode

---
+++ musicsim.py
@@ -0,0 +1,36 @@
+#Programmer Dixon Styres
+#Version 2-9-14
+#music sim
+
+#working on xquartz, pretty sure it might need other things for xming
+#also not exaclty sure what im looking at, guessing it is time vs value (i.e. pitches looks weird)
+
+from pyechonest import track
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    timbre_list = []
+    pitch_list = []
+    t = track.track_from_id('TRKPXWQ145D2A22467')
+    t.get_analysis()
+    for i in range(0,len(t.segments)):
+    	a=t.segments[i]
+        timbre_list.append( a[u'timbre'])
+    for i in range(0,len(t.segments)):
+        a=t.segments[i]
+        pitch_list.append( a[u'pitches'])
+    plot(timbre_list, "Timbre", t)
+    plot(pitch_list, "Pitch", t)
+
+def plot(list, value, track):
+    n = np.asarray(list)
+    plt.xlabel("Time")
+    plt.ylabel(value)
+    plt.title(value +" Progression : " + track.meta[u'title'])
+    plt.plot(n)
+    plt.show()
+
+
+if  __name__ =='__main__':
+    main()
